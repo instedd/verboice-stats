d04c3807ecda42ac876ce2b7acca06605c283102
nditada <nditada@gmail.com>
2014-04-25 19:04:25 -0300

    Config in environment

--- verboice-stats.js
+++ verboice-stats.js
@@ -10,7 +10,7 @@ var app = express();
 var bucket = 'verboice-stats.instedd.org';
 var localStatsPath = './stats.json';
 
-AWS.config.loadFromPath('./config.json');
+AWS.config.update({region: 'us-west-1'});
 
 var s3 = new AWS.S3(); 
 
